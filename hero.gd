f0ca7baedb1d5136c99627e3ce75ae54989cc4d7
Clemens Prerovsky <c.prerovsky@gmail.com>
2026-03-08 16:55:37 +0100

    added jumping

--- hero.gd
+++ hero.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
 const GRAVITY = 980.0
 
 @onready var body: CharacterBody2D = $CharacterBody2D
@@ -11,17 +12,31 @@ func _physics_process(delta: float) -> void:
 	if not body.is_on_floor():
 		body.velocity.y += GRAVITY * delta
 
+	# Handle jump
+	if Input.is_action_just_pressed("up") and body.is_on_floor():
+		body.velocity.y = JUMP_VELOCITY
+
 	# Get the input direction based on "left" and "right" keybindings
 	var direction := Input.get_axis("left", "right")
 	
 	if direction != 0:
 		body.velocity.x = direction * SPEED
-		anim.play("run")
-		# Flip the sprite horizontally when running left
+		# Flip the sprite horizontally when moving left
 		anim.flip_h = direction < 0
 	else:
 		body.velocity.x = move_toward(body.velocity.x, 0, SPEED)
-		anim.play("idle")
+
+	# Animation handling
+	if not body.is_on_floor():
+		if body.velocity.y < 0:
+			anim.play("jump")
+		else:
+			anim.play("jump_landing")
+	else:
+		if direction != 0:
+			anim.play("run")
+		else:
+			anim.play("idle")
 
 	# Move the character body
 	body.move_and_slide()

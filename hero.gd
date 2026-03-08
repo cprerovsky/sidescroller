4a7b5efad202859639e2f0e190fb2a9704454c93
Clemens Prerovsky <c.prerovsky@gmail.com>
2026-03-08 16:49:08 +0100

    added running left and right

--- hero.gd
+++ hero.gd
@@ -1,11 +1,27 @@
 extends Node2D
 
+const SPEED = 300.0
+const GRAVITY = 980.0
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@onready var body: CharacterBody2D = $CharacterBody2D
+@onready var anim: AnimatedSprite2D = $CharacterBody2D/AnimatedSprite2D
 
+func _physics_process(delta: float) -> void:
+	# Apply basic gravity so the character falls to the ground
+	if not body.is_on_floor():
+		body.velocity.y += GRAVITY * delta
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	# Get the input direction based on "left" and "right" keybindings
+	var direction := Input.get_axis("left", "right")
+	
+	if direction != 0:
+		body.velocity.x = direction * SPEED
+		anim.play("run")
+		# Flip the sprite horizontally when running left
+		anim.flip_h = direction < 0
+	else:
+		body.velocity.x = move_toward(body.velocity.x, 0, SPEED)
+		anim.play("idle")
+
+	# Move the character body
+	body.move_and_slide()
